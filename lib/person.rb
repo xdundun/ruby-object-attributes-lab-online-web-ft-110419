80075ff076618b1df58408331951bbd0a0bcfa0b
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-11 23:55:25 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+
+  def breed
+    @breed
+  end
+end
